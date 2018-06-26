6e3ac8e414a6eb484ef5f7aa80b6c41a3f439188
smanish01 <manish.shukla@built.io>
2018-06-26 18:51:59 +0530

    not working

--- src/Twitter.js
+++ src/Twitter.js
@@ -1,17 +1,72 @@
 import React from 'react';
 import './App.css';
-import _ from 'lodash';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 
 class Twitter extends React.Component {
 
+    componentWillMount(){
+        axios.get('http://localhost:3001/userdata')
+    }
+
+    // submitTweet() {
+    //     var tweet = document.getElementById('myTextArea').value;
+
+    //     let data = {
+    //         tweet : tweet
+    //     }
+
+    //     console.log(data)
+    //     axios.post('http://localhost:3001/tweet',data)
+    //         .then(
+    //             res => {
+    //                 console.log(res)
+
+    //                 alert('your tweet was submitted successfully')
+    //             }
+    //         )
+    //         .catch(
+    //             err => {
+    //                 console.log(err)
+    //                 alert('your tweet was not submitted successfully')
+    //             }
+    //         )
+
+    // }
+
+
+    // componentWillMount(){
+    //     axios.get('http://localhost:3001/userdata')
+    //     .then(
+    //         res => {
+    //             console.log(res.data.message)
+    //         }
+    //     )
+    //     .catch(
+    //         err => {
+    //             console.log(err)
+    //         }
+    //     )
+    // }
+
     render() {
         return (
-            <div>
-                
+            <div id='middlePageDesign'>
+                <form onSubmit={this.submitTweet}>
+                    <textarea id='myTextArea' maxLength={140}>
+                    </textarea>
+                    <br />
+                    <input type='submit' value='tweet' />
+                </form>
             </div>
+
+
+            // <div id='middlePageDesign'>
+            //         <textarea id='myTextArea' maxLength={140}>
+            //         </textarea>
+            //         <br />
+            //         <button  onClick={this.submitTweet}>tweet</button>
+            // </div>
         );
     }
 }
--- src/mw.js
+++ src/mw.js
@@ -1,24 +1,24 @@
 var express = require('express')
-var app = express()
 var bodyParser = require('body-parser')
-var axios = require('axios');
-// var mongooose = require('mongoose');
-// const MongoStore = require('connect-mongo')(session);
 var session = require('express-session')
+var app = express()
 var passport = require('passport')
     , TwitterStrategy = require('passport-twitter').Strategy;
+
 passport.use(new TwitterStrategy({
-    consumerKey: 'V0mQrAR0U4tKmVUfpLhdmwZYB',
-    consumerSecret: '5jPrblDE3R8qCd7sr0RrJ5F7zf2OlJfw9i8CiXlhWugeTqP0J8',
+    consumerKey: 'zdSLhvzisSPptRVUuWJRsd3il',
+    consumerSecret: 'lot3lRN3Re8PzucVM51mKbliBEY0Y2Rz8IFO909kUHLtQsfomw',
     callbackURL: "http://localhost:3001/auth/twitter/callback"
 },
     function (token, tokenSecret, profile, done) {
+
         if (profile) {
-
-            console.log('token here ->>>>>>>>>>>>>>>>>>>>>>>>>>', token)
-            console.log('token secret here ->>>>>>>>>>>>>>>>>>>>>>>>>>', tokenSecret)
-
-            return done(null, token, tokenSecret);
+            user = profile     
+            user.consumerKey = 'zdSLhvzisSPptRVUuWJRsd3il',
+                user.consumerSecret = 'lot3lRN3Re8PzucVM51mKbliBEY0Y2Rz8IFO909kUHLtQsfomw'
+            user.token = token
+            user.tokenSecret = tokenSecret
+            return done(null, user, token, tokenSecret);
         }
         else {
             return done(null, false);
@@ -26,15 +26,18 @@ passport.use(new TwitterStrategy({
     }
 ));
 
-passport.serializeUser(function (token, tokenSecret, done) {
-    done(null, token, tokenSecret);
+passport.serializeUser(function (user, done) {
+    done(null, user);
 });
 
-passport.deserializeUser(function (token, tokenSecret, done) {
-    done(null, token, tokenSecret);
+passport.deserializeUser(function (user, done) {
+    done(null, user);
 });
 
-// mongooose.connect('mongodb://localhost/db');
+app.use(passport.initialize());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -46,46 +49,29 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    // store: new MongoStore({ mongooseConnection: mongooose.connection }),
     cookie: { maxAge: 365 * 24 * 60 * 60 * 1000 }
 }))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(passport.initialize());
 
 app.get('/auth/twitter', passport.authenticate('twitter'));
 
-app.get('/auth/twitter/callback',
-    function (req, res, next) {
-        passport.authenticate('twitter', function (err, token, tokenSecret, info) {
+app.get('/auth/twitter/callback', function (req, res, next) {
+    passport.authenticate('twitter', function (err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.redirect('http://localhost:3000/'); }
+        req.logIn(user, function (err) {
             if (err) { return next(err); }
-            if (!token) { return res.redirect('http://localhost:3000/'); }
-            req.logIn(token,tokenSecret, function (err) {
-                if (err) { return next(err); }
 
-                // axios.post('https://api.twitter.com/1.1/statuses/update.json?status=test2345',{headers:{
-                //     'authentication': token
-                // }})
-                // .then(
-                //     res => {
-                //         console.log('res here ->>>>>>>>>>>>>>>>>>>>>..',res)
-                //     }
-                // )
-                // .catch(
+            return res.redirect('http://localhost:3000/twitter')
+        });
+    })(req, res, next);
+});
 
-                //     err => {
-                //         console.log('err here ->>>>>>>>>>>>>>>>>>>>>..',err)
-                //     }
-                // )
 
-                return res.redirect('http://localhost:3000/twitter');
-
-            });
-        })(req, res, next)
-    }
-);
+app.get('/userdata',
+    function (req, res) {
+        console.log(req.user.consumerKey)
+    });
 
 
 app.listen(3001,
